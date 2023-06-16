854766ecc8de91b2b45d7efaeec6c0ed9d09c9e2
Rodrigo Bichet <rodrigobichet.pl010@academico.ifsul.edu.br>
2023-06-16 20:00:39 -0300

    Feat: Class 3 Updated Project

--- src/App.js
+++ src/App.js
@@ -6,9 +6,25 @@ function App() {
     return (
         <div className="App">
             <div className="app_videos">
-                <Video></Video>
-                <Video></Video>
-                <Video></Video>
+                <Video
+                    likes={111}
+                    messages={222}
+                    shares={333}
+                    name="Paulo"
+                    description="Brecker o goleiro"
+                    music="musica épica"
+                    url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+                ></Video>
+                <Video
+                    likes={444}
+                    messages={555}
+                    shares={666}
+                    name="Pedro"
+                    description="Bird olhando pra camera"
+                    music="Clap your hands"
+                    url="
+                    https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"
+                ></Video>
             </div>
         </div>
     );
--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from "react";
 import "./video.css";
 import VideoFooter from "./components/footer/VideoFooter";
+import VideoSidebar from "./components/sidebar/VideoSidebar";
 
-function Video() {
+function Video({ likes, messages, shares, name, description, music, url }) {
     const videoRef = useRef(null);
     const [play, setPlay] = useState(false);
 
@@ -23,10 +24,18 @@ function Video() {
                 ref={videoRef}
                 onClick={handdleStart}
                 loop
-                src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+                src={url}
             ></video>
-            {/*Side bar */}
-            <VideoFooter></VideoFooter>
+            <VideoSidebar
+                likes={likes}
+                messages={messages}
+                shares={shares}
+            ></VideoSidebar>
+            <VideoFooter
+                name={name}
+                description={description}
+                music={music}
+            ></VideoFooter>
         </div>
     );
 }
